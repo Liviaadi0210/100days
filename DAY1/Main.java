d4de15e120d43369349781759f736b505e8e171c
Liviaadi0210 <147024843+Liviaadi0210@users.noreply.github.com>
2023-10-09 13:26:34 +0800

    Add files via upload

---
+++ DAY1/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama ="pia";
+        String nim ="D0223018";
+        int angkatan =2023;
+        System.out.println("nama :" +nama + "\n" + "nim:" + nim + "\n" + "angkatan:" + angkatan);
+        
+    }
+    
+}
